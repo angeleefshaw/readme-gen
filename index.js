d5b10c97b31c8c143ffe1f46b8e1ed7d822fa054
Angelee Shaw <angeleefshaw@gmail.com>
2020-12-30 18:16:13 -0500

    fixing issues

--- index.js
+++ index.js
@@ -1,64 +1,74 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
-const util = require('util');
+const {writeFile} = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-const markdown = require('./utils/generateMarkdown')
+//const writeFileAsync = util.promisify(fs.writeFile)
 
 // array of questions for user
-const questions = [{
-    type:"input",
-    message: "What is the title of your project?",
-    name: "Title"
-},{
-    type:"input",
-    message: "Give a short description of your project",
-    name: "Description"
-},{
-    type:"input",
-    message: "Provide a table of contents",
-    name: "Table of Contents"
-},{
-    type:"input",
-    message: "What instructions should users follow to install your application?",
-    name: "Installation"
-},{
-    type:"input",
-    message: "How to use this applicaiton?",
-    name: "Usage"
-},{
-    type:"list",
-    message: "What is the license for this project?",
-    name: "License"
-    choices: ["MIT", "APACHE 2.0", "artistic-2.0", "bsl-1.0", "None"]
-},{
-    type:"input",
-    message: "How can users contribute to this project?",
-    name: "Contributing"
-},{
-    type:"input",
-    message: "What command is needed to run tests?",
-    name: "Tests"
-},{
-    type:"input",
-    message: "What is your github username?",
-    name: "Questions"
-}
-];
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            name: "Title",
+            message: "What is the title of your project?",
+            type:"input",
+        },{
+            type:"input",
+            name: "Description",
+            message: "Give a short description of your project",
+            
+        },{
+            type:"input",
+            name: "Table of Contents",
+            message: "Provide a table of contents",
+            
+        },{
+            type:"input",
+            name: "Installation",
+            message: "What instructions should users follow to install your application?",
+            
+        },{
+            type:"input",
+            message: "How to use this applicaiton?",
+            name: "Usage"
+        },{
+            type:"list",
+            message: "What is the license for this project?",
+            name: "License",
+            choices: [
+                "MIT", 
+                "APACHE 2.0", 
+                "artistic-2.0", 
+                "bsl-1.0", 
+                "None"]
+        },{
+            type:"input",
+            message: "How can users contribute to this project?",
+            name: "Contributing"
+        },{
+            type:"input",
+            message: "What command is needed to run tests?",
+            name: "Tests"
+        },{
+            type:"input",
+            message: "What is your github username?",
+            name: "Questions"
+        },
+    ])
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+};
 
-// function to initialize program
-function init() {
-    inquirer.prompt(questions.message)
-      .then((data) => writeFileAsync('README.md', generateHTML(data)))
-      .then(() => console.log('Successfully wrote to README'))
-      .catch((err) => console.error(err));
-
-}
+    
+promptUser().then((answers) => {
+    writeFile('README.txt', generateMarkdown(answers));
+    console.log('Successfully wrote to README')
+})
+.catch((err) => console.error(err));
 
 
-// function call to initialize program
-init()
+
+
+
+
+
+
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,8 +4,18 @@ function generateMarkdown(data) {
   #Description 
   ${data.Description}
   #Table of Conntents
+  1. [Installation](#Installation)
+  2. [Usage](#Usage)
+  3. [License](#license)
+  4. [Contributing](#Contributing)
+  5. [Tests](#Tests)
+  6. [Questions](#Questions)
+  ## Installation
+  ${data.Installation}
+  ## Usage
+  ${data.Usage}
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown};
